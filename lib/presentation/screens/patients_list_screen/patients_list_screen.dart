c987b6f7945ee8081d0169b32c6e92fbc6a349c7
Pablo Darre <pablo@hattrick-it.com>
2021-07-27 11:46:35 -0300

    updated patients screen

--- lib/presentation/screens/patients_list_screen/patients_list_screen.dart
+++ lib/presentation/screens/patients_list_screen/patients_list_screen.dart
@@ -1,100 +1,205 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../domain/entities/chat_user.dart';
-import '../channels_list/channels_list_screen.dart';
-import '../../viewmodel/auth_viewmodel/auth_viewmodel.dart';
-import '../../viewmodel/user_selection_viewmodel/user_selection_viewmodel.dart';
+import 'package:flutter_advanced_avatar/flutter_advanced_avatar.dart';
+import 'package:flutter_riverpod/all.dart';
+import 'package:sendbirdtutorial/Core/chat_colors.dart';
+import 'package:sendbirdtutorial/domain/entities/chat_user.dart';
+import 'package:sendbirdtutorial/presentation/screens/chat_screen/chat_screen.dart';
+import 'package:sendbirdtutorial/presentation/screens/common_widgets/common_appbar.dart';
+import 'package:sendbirdtutorial/presentation/viewmodel/user_selection_viewmodel/user_selection_viewmodel.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sendbirdtutorial/presentation/viewmodel/users_list_viewmodel/users_list_viewmodel.dart';
 
 class PatientsListScreen extends StatelessWidget {
-  static const String routeName = '/patients-list-screen';
-
-  const PatientsListScreen();
+  static const String routeName = '/patient-list-screen';
 
   @override
   Widget build(BuildContext context) {
-    final userType = ModalRoute.of(context).settings.arguments as String;
-    context.read(userSelectionViewModel).saveUserType(userType);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read(userSelectionViewModel).getUsersByType();
+    });
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-            '${AppLocalizations.of(context).userSelectionPageAppBarTitle} $userType'),
+      appBar: CommonAppbar(
+        title: AppLocalizations.of(context).patientListScreenAppbarTitle,
+        appbarColor: ChatColors.greyAppbarBackgroundColor,
       ),
-      body: Consumer(
-        builder: (context, watch, child) {
-          final providerState = watch(authViewModel).getState;
-          if (providerState == LoginState.Empty) {
-            return BuildUserSelectionBody(userType: userType);
-          } else if (providerState == LoginState.Loaded) {
-            WidgetsBinding.instance?.addPostFrameCallback((_) {
-              Navigator.of(context)
-                  .popAndPushNamed(ChannelListScreen.routeName);
-            });
-          }
-          return Center(child: CircularProgressIndicator());
-        },
-      ),
+      body: BuildPatientsListBody(),
     );
   }
 }
 
-class BuildUserSelectionBody extends ConsumerWidget {
-  final String userType;
-
-  const BuildUserSelectionBody({this.userType});
-
-  @override
-  Widget build(BuildContext context, watch) {
-    return FutureBuilder(
-      future: context.read(userSelectionViewModel).getUsersByType(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          List<ChatUser> list = snapshot.data;
-          return ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (context, index) {
-              return SlideInRight(
-                delay: Duration(milliseconds: index * 150),
-                from: 350,
-                child: BuildUserCard(chatUser: list[index]),
-              );
-            },
-          );
-        }
-        return Center(child: Center(child: CircularProgressIndicator()));
-      },
-    );
-  }
-}
-
-class BuildUserCard extends StatelessWidget {
-  final ChatUser chatUser;
-
-  const BuildUserCard({this.chatUser});
+class BuildPatientsListBody extends StatelessWidget {
+  const BuildPatientsListBody();
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () async {
-      },
-      child: ListTile(
-        leading: CircleAvatar(
-          maxRadius: 26,
-          child: CircleAvatar(
-            maxRadius: 25,
-            backgroundImage: NetworkImage(chatUser.profileUrl),
-          ),
+    return Column(
+      children: [
+        SearchComponent(),
+
+        Consumer(
+          builder: (context, watch, child) {
+            var provider = watch(userSelectionViewModel);
+            if (provider.getUserList == null) {
+              return Center(child: CircularProgressIndicator());
+            } else if (provider.getUserSelectionSatus ==
+                UserSelectionStatus.Loaded) {
+              return PatientsList(userList: provider.getUserList);
+            }
+            return Center(
+              child: Container(
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                ),
+              ),
+            );
+          },
         ),
-        title: chatUser.nickname != null || chatUser.nickname.isNotEmpty
-            ? Text(chatUser.nickname)
-            : Text(chatUser.userId),
-        subtitle: chatUser
-                    .metadata[AppLocalizations.of(context).doctorSpecialty] !=
-                null
-            ? Text(
-                chatUser.metadata[AppLocalizations.of(context).doctorSpecialty])
-            : Text(''),
+      ],
+    );
+  }
+}
+
+class SearchComponent extends StatelessWidget {
+  const SearchComponent();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: double.infinity,
+      color: ChatColors.greyAppbarBackgroundColor,
+      child: Column(
+        children: [
+          BuildSearchBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class BuildSearchBar extends StatelessWidget {
+  const BuildSearchBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350,
+      height: 50,
+      color: ChatColors.greyAppbarBackgroundColor,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        padding: EdgeInsets.only(left: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: ChatColors.whiteColor,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: TextField(
+                style:
+                TextStyle(color: ChatColors.disbleSendButton, fontSize: 12),
+                cursorColor: ChatColors.disbleSendButton,
+                onChanged: (val) {
+                  context.read(userSelectionViewModel).getUserByName(val);
+                },
+                decoration: InputDecoration(
+                  hintText:
+                  AppLocalizations.of(context).doctorListScreenHinttext,
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            Icon(
+              Icons.search,
+              color: ChatColors.doctorSearchBar,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class PatientsList extends StatelessWidget {
+  final List<ChatUser> userList;
+
+  const PatientsList({this.userList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.separated(
+        physics: BouncingScrollPhysics(),
+        itemCount: userList.length,
+        shrinkWrap: true,
+        separatorBuilder: (context, index) {
+          return Divider(
+            thickness: 1,
+          );
+        },
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () async {
+              var gropuChannel = await context
+                  .read(usersListViewModel)
+                  .getChannelByIds(userList[index].userId);
+              if (gropuChannel != null) {
+                Navigator.of(context)
+                    .pushNamed(ChatScreen.routeName, arguments: gropuChannel);
+              }
+            },
+            child: Container(
+              width: double.infinity,
+              height: 90,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 20, top: 5),
+                    child: AdvancedAvatar(
+                      image: NetworkImage(userList[index].profileUrl),
+                      foregroundDecoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: ChatColors.greyAppbarBackgroundColor,
+                          width: 1.0,
+                        ),
+                      ),
+                      size: 60,
+                      statusSize: 16,
+                      statusColor: userList[index].isOnline
+                          ? ChatColors.doctorAvailable
+                          : ChatColors.doctorNotAvailable,
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          userList[index].metadata['Specialty'] != null
+                              ? Text(
+                            userList[index].metadata['Specialty'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                              : Container(),
+                          Text(
+                            userList[index].nickname,
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
